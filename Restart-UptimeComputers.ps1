7d0a9ab9ad9a3fde44af66e8639c88065f2f1833
RachelCatchesFord <71400422+RachelCatchesFord@users.noreply.github.com>
2020-12-18 13:52:20 -0700

    Update Restart-UptimeComputers.ps1

--- Restart-UptimeComputers.ps1
+++ Restart-UptimeComputers.ps1
@@ -9,6 +9,6 @@ $Uptime = (Get-Date) - $OS.ConvertToDateTime($OS.LastBootUpTime)
 
 if($DaysUp -ge $Days){
     $Tomorrow = (Get-Date).AddDays(1).Date.AddHours($Time)
-    msg.exe * "Your computer has been up for $($DaysUp) Days. Scheduling a restart for $($Tomorrow)."
+    msg.exe * "***OIT DESKSIDE SUPPORT*** Your computer has been up for $($DaysUp) Days. Scheduling a restart for $($Tomorrow)."
     shutdown -r -t ([decimal]::round(($Tomorrow - (Get-Date)).TotalSeconds))
 }
