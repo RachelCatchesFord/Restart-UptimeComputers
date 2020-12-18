118b9fcc64d65f168d9eaf2133ccfc02a93e362e
RachelCatchesFord <71400422+RachelCatchesFord@users.noreply.github.com>
2020-12-18 10:09:23 -0700

    Added already created scripts

--- Restart-UptimeComputers.ps1
+++ Restart-UptimeComputers.ps1
@@ -1,3 +1,7 @@
 Param(
     [parameter(Mandatory)][int]$Days
 )
+
+$OS = Get-wmiobject Win32_OperatingSystem
+$Uptime = (Get-Date) - $OS.ConvertToDateTime($OS.LastBootUpTime)
+$DaysUp = $Uptime.TotalDays
