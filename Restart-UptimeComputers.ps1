b536fc655e1c24b27d41a0c268faf02ca5472d73
RachelCatchesFord <71400422+RachelCatchesFord@users.noreply.github.com>
2020-12-18 10:31:40 -0700

    Schedule restart using shutdown -r -t

--- Restart-UptimeComputers.ps1
+++ Restart-UptimeComputers.ps1
@@ -1,7 +1,12 @@
 Param(
-    [parameter(Mandatory)][int]$Days
+    [parameter(Mandatory)][int]$Days,
+    [parameter(Mandatory)][int]$Hours
 )
 
 $OS = Get-wmiobject Win32_OperatingSystem
 $Uptime = (Get-Date) - $OS.ConvertToDateTime($OS.LastBootUpTime)
-$DaysUp = $Uptime.TotalDays
+[int]$DaysUp = $Uptime.TotalDays
+
+if($DaysUp -ge $Days){
+    shutdown -r -t ([decimal]::round(((Get-Date).AddDays(1).Date.AddHours($Hours) - (Get-Date)).TotalSeconds))
+}
