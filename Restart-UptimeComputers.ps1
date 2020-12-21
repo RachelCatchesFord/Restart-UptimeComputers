6c18deeb05dff8ca20855535075573a10ebd83a3
brandonjkessler <brandon.j.kessler@imagineink.net>
2020-12-21 13:32:23 -0700

    Made line 11 a string, because strings

--- Restart-UptimeComputers.ps1
+++ Restart-UptimeComputers.ps1
@@ -8,7 +8,7 @@ $Uptime = (Get-Date) - $OS.ConvertToDateTime($OS.LastBootUpTime)
 [int]$DaysUp = $Uptime.TotalDays
 
 if($DaysUp -ge $Days){
-    $SupportGroup = OIT DESKSIDE SUPPORT
+    $SupportGroup = 'OIT DESKSIDE SUPPORT'
     $Tomorrow = (Get-Date).AddDays(1).Date.AddHours($Time)
     msg.exe * "***$($SupportGroup)*** Your computer has been up for $($DaysUp) Days. Scheduling a restart for $($Tomorrow)."
     shutdown -r -t ([decimal]::round(($Tomorrow - (Get-Date)).TotalSeconds))
