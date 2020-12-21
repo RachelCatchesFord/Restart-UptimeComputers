41ef43d67c1e21b173a0da58736fc2d4308ab7ec
brandonjkessler <brandon.j.kessler@imagineink.net>
2020-12-21 13:28:12 -0700

    Made a bit more modular

--- Restart-UptimeComputers.ps1
+++ Restart-UptimeComputers.ps1
@@ -8,7 +8,8 @@ $Uptime = (Get-Date) - $OS.ConvertToDateTime($OS.LastBootUpTime)
 [int]$DaysUp = $Uptime.TotalDays
 
 if($DaysUp -ge $Days){
+    $SupportGroup = OIT DESKSIDE SUPPORT
     $Tomorrow = (Get-Date).AddDays(1).Date.AddHours($Time)
-    msg.exe * "***OIT DESKSIDE SUPPORT*** Your computer has been up for $($DaysUp) Days. Scheduling a restart for $($Tomorrow)."
+    msg.exe * "***$($SupportGroup)*** Your computer has been up for $($DaysUp) Days. Scheduling a restart for $($Tomorrow)."
     shutdown -r -t ([decimal]::round(($Tomorrow - (Get-Date)).TotalSeconds))
 }
