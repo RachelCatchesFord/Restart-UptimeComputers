7f7197dcc074b5385acd5815f044493fec3ab76e
Brandon Kessler <brandon.kessler@state.co.us>
2021-01-14 16:17:36 -0700

    Added logic to see if user is logged in

--- Deploy-FromSCCM.ps1
+++ Deploy-FromSCCM.ps1
@@ -1,6 +1,17 @@
 Param(
-    [Parameter(Mandatory = $true,Position = 0)][String]$Destination
+    [Parameter(Mandatory = $true,Position = 0)][String]$Destination,
+    [Parameter(Mandatory = $true,Position = 1)][String]$Domain
 )
 
-
+## Copy PSADT to local machine
 Copy-Item -Path . -Destination $Destination -Force -Recurse
+
+
+## Detect Logged on Users
+$LoggedOnUsers = Get-Process -IncludeUserName | Select-Object UserName,SessionId | Where-Object {($_.UserName -ne $null) -and ($_.UserName -like "$Domain*")} | Sort-Object SessionId -Unique
+
+if($LoggedOnUsers){
+    Return $true
+}
+
+## Scheduled Task goes here
